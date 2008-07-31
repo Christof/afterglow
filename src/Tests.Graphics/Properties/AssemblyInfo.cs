79072ee9efe179cf13bdd60d98a7c3134b6c0393
Christof Sirk <sirchb01@gmail.com>
2008-07-31 15:36:00 +0200

    -NEW: Added Graphics proejct and test project

---
+++ src/Tests.Graphics/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tests.Graphics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests.Graphics")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1bd12387-dbae-4f81-854b-f88d617beabc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Tests.Tests.Graphics")]
---
+++ src/Tests.Graphics/TestRenderWindow.cs
@@ -0,0 +1,15 @@
+using System;
+using MbUnit.Framework;
+
+namespace TheNewEngine.Graphics
+{
+    [TestFixture]
+    public class TestRenderWindow
+    {
+        [Test]
+        public void CanCreate()
+        {
+           
+        }
+    }
+}
