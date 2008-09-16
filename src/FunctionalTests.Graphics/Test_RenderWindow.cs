f7c9693bc0df695517355bef42e0763e690d3392
Christof Sirk <sirchb01@gmail.com>
2008-09-16 22:13:32 +0200

    added FuncitonalTests.Graphics projects and moved functional tests from Tests.Graphics over

---
+++ src/FunctionalTests.Graphics/Test_RenderWindow.cs
@@ -0,0 +1,109 @@
+using System.Drawing;
+using System.Windows.Forms;
+using MbUnit.Framework;
+using System;
+
+namespace TheNewEngine.Graphics
+{
+    [TestFixture]
+    public class Test_RenderWindow
+    {
+        [Test]
+        public void CanSwitchBetweenRenderWindows()
+        {
+            using (var form = new Form())
+            {
+                form.ClientSize = new Size(800, 600);
+
+                IRenderWindow renderWindow = new Xna.RenderWindow(form.Handle);
+
+                form.KeyPress +=
+                    delegate(object sender, KeyPressEventArgs args)
+                    {
+                        if (args.KeyChar == 'p')
+                        {
+                            renderWindow.TakeScreenshot("test.bmp");
+                        }
+                        else if (args.KeyChar == 's')
+                        {
+                            if (renderWindow is Xna.RenderWindow)
+                            {
+                                renderWindow.Dispose();
+                                renderWindow = new SlimDX.RenderWindow(form.Handle);
+                            }
+                            else
+                            {
+                                renderWindow.Dispose();
+                                renderWindow = new Xna.RenderWindow(form.Handle);
+                            }
+                        }
+                    };
+
+                Application.Idle +=
+                    delegate
+                    {
+                        renderWindow.Render();
+
+                        Application.DoEvents();
+                    };
+
+                Application.Run(form);
+
+                if (renderWindow != null)
+                {
+                    renderWindow.Dispose();
+                }
+            }
+        }
+
+        [Test]
+        public void CanSwitchBetweenRenderWindowsWhenRenderingIsDoneInAControl()
+        {
+            using (var form = new Form())
+            {
+                form.ClientSize = new Size(1024, 760);
+
+                var button = new Button { Size = new Size(800, 600), Top = 112, Left = 80, Parent = form };
+
+                IntPtr handle = button.Handle;
+                IRenderWindow renderWindow = new Xna.RenderWindow(handle);
+
+                button.KeyPress +=
+                    delegate(object sender, KeyPressEventArgs args)
+                    {
+                        if (args.KeyChar == 'p')
+                        {
+                            renderWindow.TakeScreenshot("test.bmp");
+                        }
+                        else if (args.KeyChar == 's')
+                        {
+                            if (renderWindow is Xna.RenderWindow)
+                            {
+                                renderWindow.Dispose();
+                                renderWindow = new SlimDX.RenderWindow(handle);
+                            }
+                            else
+                            {
+                                renderWindow.Dispose();
+                                renderWindow = new Xna.RenderWindow(handle);
+                            }
+                        }
+                    };
+
+                Application.Idle +=
+                    delegate
+                    {
+                        renderWindow.Render();
+
+                        Application.DoEvents();
+                    };
+                Application.Run(form);
+
+                if (renderWindow != null)
+                {
+                    renderWindow.Dispose();
+                }
+            }
+        }
+    }
+}
