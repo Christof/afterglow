91d492a8a1c18b52fff0c5e562cfa912cebf341f
Christof Sirk <sirchb01@gmail.com>
2008-09-21 17:26:57 +0200

    added comparison test to evaluate the camera functionality

---
+++ src/Tests.Graphics.Xna/Comparison/Test_Comparison_PerspectiveProjectionLense.cs
@@ -0,0 +1,26 @@
+using MbUnit.Framework;
+using TheNewEngine.Graphics.Xna;
+
+namespace TheNewEngine.Graphics.Cameras
+{
+    public class Test_Comparison_PerspectiveProjectionLense
+    {
+        [Test]
+        public void CalculateProjectionMatrix()
+        {
+            var distanceToNearPlane = 0.001f;
+            var distanceToFarPlane = 100000f;
+            var horizontalFieldOfView = 0.75f;
+            var aspectRatio = 4.0f / 3.0f;
+
+            var projection = PerspectiveProjectionLense.CalculateProjectionMatrix(
+                distanceToNearPlane, distanceToFarPlane, horizontalFieldOfView, aspectRatio);
+
+            var projectionXna = Microsoft.Xna.Framework.Matrix.CreatePerspectiveFieldOfView(
+                horizontalFieldOfView / aspectRatio, aspectRatio, 
+                distanceToNearPlane, distanceToFarPlane);
+
+            Assert.AreEqual(projectionXna, projection.ToXna());
+        }
+    }
+}
