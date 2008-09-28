fb34bfaa20e52511da0d6c280c2210850a95d12b
Christof Sirk <sirchb01@gmail.com>
2008-09-28 13:45:07 +0200

    updated rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,6 @@
 buildDir = "bin"
 DOT_NET_PATH = "C:\\Windows\\Microsoft.net\\framework\\v3.5\\"
+GALLIO_PATH = "C:\\Program Files\\Gallio\\bin\\"
 
 task :default => [:build]
 #task :all => [:clear, :removeBuildDir, :removeVsDirs, :default]
@@ -16,6 +17,17 @@ end
 directory buildDir
 
 task :build => [buildDir] do
-  sh "echo build"
   sh DOT_NET_PATH + "msbuild.exe /property:Configuration=Release TheNewEngine.sln"
+end
+
+task :test do #=> :build do
+  sh GALLIO_PATH + "Gallio.Echo.exe bin/Tests.Math.dll /working-directory:bin /report-directory:build /report-type:Html /show-reports"
+end
+
+task :t do
+  sh GALLIO_PATH + "Gallio.Echo.exe TheNewEngine.gallio"
+end
+
+task :env do
+    ENV.each {|k,v| puts "#{k}=#{v}" }
 end
