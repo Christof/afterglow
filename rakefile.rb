94ce2e14552663509e3f883a29bd8273a34cc7fe
unknown <Christof@.(none)>
2008-10-15 16:44:01 +0200

    updated rakefile with test execution support and replacing _ with spaces in test names

--- rakefile.rb
+++ rakefile.rb
@@ -1,33 +1,56 @@
-buildDir = "bin"
-DOT_NET_PATH = "C:\\Windows\\Microsoft.net\\framework\\v3.5\\"
+BUILD_DIR = "bin"
+DOT_NET_PATH = "#{ENV['windir']}\\Microsoft.net\\framework\\v3.5\\"
 GALLIO_PATH = "C:\\Program Files\\Gallio\\bin\\"
+SOLUTION_NAME = "TheNewEngine.sln"
 
 task :default => [:build]
-#task :all => [:clear, :removeBuildDir, :removeVsDirs, :default]
-
-task :clear do
-  #sh "clear"
-end
+#task :all => [ :removeBuildDir, :removeVsDirs, :default]
 
 task :removeBuildDir => :clear do
   sh "echo removeBuildDir"
-  rm_rf(buildDir)
+  rm_rf(BUILD_DIR)
 end
 
-directory buildDir
+directory BUILD_DIR
 
-task :build => [buildDir] do
-  sh DOT_NET_PATH + "msbuild.exe /property:Configuration=Release TheNewEngine.sln"
+task :build => [BUILD_DIR] do
+  sh "#{DOT_NET_PATH}msbuild.exe /property:Configuration=Release #{SOLUTION_NAME}"
 end
 
 task :test do #=> :build do
-  sh GALLIO_PATH + "Gallio.Echo.exe bin/Tests.Math.dll /working-directory:bin /report-directory:build /report-type:Html /show-reports"
+  gallio_path = GALLIO_PATH + "Gallio.Echo.exe"
+  if (File.exist?(gallio_path))
+	print "starting tests..."
+    sh "\"#{gallio_path}\" #{BUILD_DIR}/Tests.Math.dll /working-directory:bin /report-directory:build /report-type:Html" #/show-reports
+  end 
+  
+  dir = Dir.new("build")
+  
+  entries = dir.entries.find_all do	|filename|
+	/html/ =~ filename
+  end
+  
+  newest_file = entries.max do |a, b|
+	File.new("build\\#{a}").atime <=> File.new("build\\#{b}").atime
+  end
+  
+  print "\nnewest file #{newest_file}\n"
+  
+  replace_underlines("build\\#{newest_file}")
 end
 
-task :t do
-  sh GALLIO_PATH + "Gallio.Echo.exe TheNewEngine.gallio"
-end
-
-task :env do
-    ENV.each {|k,v| puts "#{k}=#{v}" }
+def replace_underlines(path)
+	file = File.new(path, "r")
+	lines = file.readlines
+	file.close
+	
+	lines.each do |line|
+		line.gsub!("_", " ")
+	end
+	
+	file = File.new(path, "w")
+	lines.each do |line|
+		file.write(line)
+	end
+	file.close
 end
