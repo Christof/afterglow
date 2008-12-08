8b080801366ee535eb90b9be7a4810af6c93070c
Christof Sirk <sirchb01@gmail.com>
2008-12-08 18:48:30 +0100

    Worked on the test rake task.

--- rakefile.rb
+++ rakefile.rb
@@ -10,42 +10,47 @@ task :default => [:build]
 #task :all => [ :removeBuildDir, :removeVsDirs, :default]
 
 task :removeBuildDir => :clear do
-  sh "echo removeBuildDir"
-  rm_rf(BUILD_DIR)
+	  sh "echo removeBuildDir"
+	  rm_rf(BUILD_DIR)
 end
 
 directory BUILD_DIR
 
 task :build => [BUILD_DIR] do
-  sh "#{DOT_NET_PATH}msbuild.exe /property:Configuration=Release #{SOLUTION_NAME}"
+	  sh "#{DOT_NET_PATH}msbuild.exe /property:Configuration=Release #{SOLUTION_NAME}"
 end
 
-task :test => :build do
-  gallio_path = GALLIO_PATH + "Gallio.Echo.exe"
-  if (File.exist?(gallio_path))
-		print "starting tests..."
-    cmd = "\"#{gallio_path}\" #{BUILD_DIR}/Tests.Graphics.SlimDX.dll /working-directory:bin /report-directory:build /report-type:Html" #/show-reports
-		print cmd
+task :test do#=> :build do
+	gallio_path = GALLIO_PATH + "Gallio.Echo.exe"
+	if (File.exist?(gallio_path))
+		puts "starting tests..."
+		assemblies = ""
+		Dir["#{BUILD_DIR}/Tests.*.dll"].each do |path|
+			assemblies << path << " "
+		end
+		cmd = "\"#{gallio_path}\" #{assemblies} /filter:not(Type:TriangleWithTexture) " + 
+			" /working-directory:bin /report-directory:build /report-type:Html" #/show-reports
+		puts cmd
 		begin
 			sh cmd
 		rescue => err
 			puts err
 		end
-  end 
-  
-  dir = Dir.new("build")
-  
-  entries = dir.entries.find_all do	|filename|
-	/html/ =~ filename
-  end
-  
-  newest_file = entries.max do |a, b|
-	File.new("build\\#{a}").atime <=> File.new("build\\#{b}").atime
-  end
-  
-  print "\nnewest file #{newest_file}\n"
-  
-  replace_underlines("build\\#{newest_file}")
+	end
+	
+	dir = Dir.new("build")
+	
+	entries = dir.entries.find_all do	|filename|
+		/html/ =~ filename
+	end
+	
+	newest_file = entries.max do |a, b|
+		File.new("build\\#{a}").atime <=> File.new("build\\#{b}").atime
+	end
+	
+	print "\nnewest file #{newest_file}\n"
+	
+	replace_underlines("build\\#{newest_file}")
 end
 
 def replace_underlines(path)
@@ -106,5 +111,5 @@ task :fixRef do
 		s.gsub!('&apos;', '\'')
 		file.write(s)
 		file.close
-	end		
+	end
 end
