d7c3bd578f205ae94f216162c9d31a46cf8b474b
Christof Sirk <sirchb01@gmail.com>
2009-02-05 16:44:30 +0100

    Added input projects.

---
+++ src/Tests.Input/Test_InputHandler.cs
@@ -0,0 +1,16 @@
+using MbUnit.Framework;
+
+namespace TheNewEngine.Tests.Input
+{
+    public class Test_InputHandler
+    {
+        [Test]
+        public void Name()
+        {
+//            var inputDevices = new InputDevices();
+//
+//            inputDevices.Keyboard.On(Button.W).IsDown()
+//                .Do(camera => camera.MoveForward(timeSinceLastFrame));
+        }
+    }
+}
