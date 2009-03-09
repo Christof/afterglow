f717fb9f73cb4f733f3cb366ff8e210fec4083cf
Josef Koller <joe@Debian-40-etch-64-LAMP.(none)>
2009-03-09 10:15:16 +0100

    added link to wikipedia description of these mathematical constructs

--- src/Math/Primitives/Quaternion.cs
+++ src/Math/Primitives/Quaternion.cs
@@ -2,7 +2,8 @@ namespace Afterglow.Math
 {
     /// <summary>
     /// Represents a rotation in 3d-space with four values (a rotation axis and an angle).
-    /// </summary>
+    /// http://en.wikipedia.org/wiki/Quaternion
+    ///</summary>
     public class Quaternion
     {
         private Vector4 mValues;
