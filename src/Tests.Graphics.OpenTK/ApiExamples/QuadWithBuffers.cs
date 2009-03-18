ce47eb22f84b49ddb6153b06d8ebba51b346f8b5
Christof Sirk <sirchb01@gmail.com>
2009-03-18 16:41:06 +0100

    Worked on the QuadWithBuffers example for OpenTK which is not working...

--- src/Tests.Graphics.OpenTK/ApiExamples/QuadWithBuffers.cs
+++ src/Tests.Graphics.OpenTK/ApiExamples/QuadWithBuffers.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using MbUnit.Framework;
 using OpenTK;
@@ -40,35 +41,41 @@ namespace Afterglow.Graphics.OpenTK.ApiExamples
                 //have to set this to get a -1 to 1 system view
                 GL.Viewport(0, 0, WIDTH, HEIGHT);
 
+                var mPositions = new[] {new Vector3(-1, -1, 0), new Vector3(1, -1, 0), new Vector3(1, 1, 0),};
+                var mColors = new[] {new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1),};
                 //lets create 2 buffers and get their ids
                 vertexBufferIds = new uint[2];
                 GL.GenBuffers(2, vertexBufferIds);
-                
-                var mPositions = new[]
-                {
-                    new Vector3(-1, -1, 0),
-                    new Vector3(1, -1, 0),
-                    new Vector3(1, 1, 0),
-                    new Vector3(-1, 1, 0),
-                };
-                var mColors = new[]
-                {
-                    Color.Red, Color.Green,
-                    Color.Blue, Color.Yellow
-                };
 
-                Application.Idle +=
-                    delegate
-                    {
-                        mGLControl.MakeCurrent();
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, vertexBufferIds[0]);
+                GL.BufferData(
+                    BufferTarget.ElementArrayBuffer, (IntPtr)(3 * 3 * sizeof (float)), mPositions, BufferUsageHint.StreamDraw);
 
-                        GL.ClearColor(mGLControl.BackColor);
-                        GL.Clear(ClearBufferMask.ColorBufferBit);
+                //colors
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, vertexBufferIds[1]);
+                GL.BufferData(
+                    BufferTarget.ElementArrayBuffer, (IntPtr)(3 * 3 * sizeof(float)), mColors, BufferUsageHint.StreamDraw);
 
-                        RenderFrame();
+                //GL.BufferData(
+                //    BufferTarget.ArrayBuffer,
+                //    new IntPtr(6 * sizeof (ushort)),
+                //    new ushort[]
+                //    {0, 1, 3, 0, 3, 2},
+                //    BufferUsageHint.StreamDraw
+                //    );
 
-                        mGLControl.SwapBuffers();
-                    };
+
+                Application.Idle += delegate
+                                    {
+                                        mGLControl.MakeCurrent();
+
+                                        GL.ClearColor(mGLControl.BackColor);
+                                        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                                        RenderFrame();
+
+                                        mGLControl.SwapBuffers();
+                                    };
 
                 form.BringToFront();
                 Application.Run(form);
@@ -81,14 +88,35 @@ namespace Afterglow.Graphics.OpenTK.ApiExamples
         private void RenderFrame()
         {
             GL.EnableClientState(EnableCap.VertexArray);
+            GL.EnableClientState(EnableCap.ColorArray);
 
-            GL.Begin(BeginMode.Quads);
+            GL.Begin(BeginMode.Triangles);
 
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferIds[0]);
-   
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, vertexBufferIds[1]);
+            GL.ColorPointer(3, ColorPointerType.Float, 0, null);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, vertexBufferIds[0]);
+            GL.VertexPointer(3, VertexPointerType.Float, 0, null);
+
+
+            GL.DrawArrays(BeginMode.Triangles, 0, 3);
+
+            //GL.BindBuffer
+            //    (
+            //    BufferTarget.ArrayBuffer
+            //    ,
+            //    vertexBufferIds[
+            //        1]);
+
+            //disabling these buffers...
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            //render other things here...
+
+            GL.DisableClientState(EnableCap.ColorArray);
+            GL.DisableClientState(EnableCap.VertexArray);
+
             GL.End();
         }
-
 
         /// <summary>
         /// Creates a form with a set client size.
@@ -96,11 +124,7 @@ namespace Afterglow.Graphics.OpenTK.ApiExamples
         /// <returns>The new form.</returns>
         public static Form CreateForm()
         {
-            return new Form
-            {
-                ClientSize = new Size(WIDTH, HEIGHT),
-                Visible = true
-            };
+            return new Form {ClientSize = new Size(WIDTH, HEIGHT), Visible = true};
         }
     }
 }
