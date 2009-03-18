9495645bec562cff0dfd1b7f41c8eb5b5c233d36
Josef Koller <josef.koller@sperlingweg.net>
2009-03-18 10:59:32 +0100

    Finished pure OpenTK Simple Triangle Temo

---
+++ src/Tests.Graphics.OpenTK/ApiExamples/EmptyWindow.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using MbUnit.Framework;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace Afterglow.Graphics.OpenTK.ApiExamples
+{
+    [TestFixture]
+    public class EmptyWindow
+    {
+        private const int WIDTH = 800;
+        private const int HEIGHT = 600;
+
+        [Test]
+        [Category(Categories.API_EXAMPLES)]
+        public void Run()
+        {
+            using (var form = CreateForm())
+            {
+                GLControl mGLControl = new GLControl
+                {
+                    Anchor = (((((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right))),
+                    BackColor = Color.LightGreen,
+                    Location = new Point(1, 0),
+                    Name = "GL Control",
+                    Size = new Size(WIDTH, HEIGHT),
+                    TabIndex = 0,
+                    VSync = false
+                };
+                form.Controls.Add(mGLControl);
+
+                Application.Idle +=
+                    delegate
+                    {
+
+                        mGLControl.MakeCurrent();
+
+                        GL.ClearColor(mGLControl.BackColor);
+                        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                        mGLControl.SwapBuffers();
+                    };
+
+                Application.Run(form);
+            }
+        }
+       
+        /// <summary>
+        /// Creates a form with a set client size.
+        /// </summary>
+        /// <returns>The new form.</returns>
+        public static Form CreateForm()
+        {
+            return new Form
+            {
+                ClientSize = new System.Drawing.Size(WIDTH, HEIGHT)
+            };
+        }
+    }
+}
---
+++ src/Tests.Graphics.OpenTK/ApiExamples/SimpleTriangle.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MbUnit.Framework;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Math;
+
+namespace Afterglow.Graphics.OpenTK.ApiExamples
+{
+    [TestFixture]
+    public class SimpleTriangle
+    {
+        private const int HEIGHT = 600;
+
+        private const int WIDTH = 800;
+
+        private Color[] mColors;
+
+        private Vector3[] mPositions;
+
+        [Test]
+        [Category(Categories.API_EXAMPLES)]
+        public void Run()
+        {
+            using (Form form = CreateForm())
+            {
+                var mGLControl = new GLControl
+                {
+                    Anchor = (((((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right))),
+                    BackColor = Color.LightGreen,
+                    Location = new Point(1, 0),
+                    Name = "GL Control",
+                    Size = new Size(WIDTH, HEIGHT),
+                    TabIndex = 0,
+                    VSync = false
+                };
+                form.Controls.Add(mGLControl);
+
+                //have to set this to get a -1 to 1 system view
+                GL.Viewport(0, 0, WIDTH, HEIGHT);
+
+                mPositions = new[]
+                {
+                    new Vector3(-1, -1, 0),
+                    new Vector3(1, -1, 0),
+                    new Vector3(0, 1, 0)
+                };
+                mColors = new[]
+                {
+                    Color.Red, Color.Green,
+                    Color.Blue
+                };
+
+                Application.Idle +=
+                    delegate
+                    {
+                        mGLControl.MakeCurrent();
+
+                        GL.ClearColor(mGLControl.BackColor);
+                        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                        RenderFrame();
+
+                        mGLControl.SwapBuffers();
+                    };
+
+                form.BringToFront();
+                Application.Run(form);
+            }
+        }
+            
+        private void RenderFrame()
+        {
+            GL.Begin(BeginMode.Triangles);
+
+            RenderVertex(0);
+            RenderVertex(1);
+            RenderVertex(2);
+
+            GL.End();
+        }
+
+        private void RenderVertex(int index)
+        {
+            RenderVertex(mPositions[index], mColors[index]);
+        }
+
+        private void RenderVertex(Vector3 position, Color color)
+        {
+            GL.Color4(color);
+            GL.Vertex3(position);
+        }
+
+        /// <summary>
+        /// Creates a form with a set client size.
+        /// </summary>
+        /// <returns>The new form.</returns>
+        public static Form CreateForm()
+        {
+            return new Form
+            {
+                ClientSize = new Size(WIDTH, HEIGHT),
+                Visible = true
+            };
+        }
+    }
+}
