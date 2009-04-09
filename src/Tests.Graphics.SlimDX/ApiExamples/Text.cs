cf24f25a96fc70ce8a7358174d7562439147036d
Christof Sirk <sirchb01@gmail.com>
2009-04-09 19:40:10 +0200

    Added text rendering using DirectWrite. The example has problems with the combination of Direct3D and DirectWrite.

--- src/Tests.Graphics.SlimDX/ApiExamples/Text.cs
+++ src/Tests.Graphics.SlimDX/ApiExamples/Text.cs
@@ -3,10 +3,16 @@ using System.Windows.Forms;
 using Afterglow.Infrastructure;
 using MbUnit.Framework;
 using SlimDX;
+using SlimDX.Direct2D;
 using SlimDX.Direct3D10;
+using SlimDX.DirectWrite;
 using SlimDX.DXGI;
 using Device = SlimDX.Direct3D10.Device;
 using Font = SlimDX.Direct3D10.Font;
+using DirectWriteFactory = SlimDX.DirectWrite.Factory;
+using Direct2DFactory = SlimDX.Direct2D.Factory;
+using FontWeight = SlimDX.DirectWrite.FontWeight;
+using FontStyle = SlimDX.DirectWrite.FontStyle;
 
 namespace Afterglow.Graphics.SlimDX.ApiExamples
 {
@@ -17,39 +23,97 @@ namespace Afterglow.Graphics.SlimDX.ApiExamples
         private const int WIDTH = 800;
         private const int HEIGHT = 600;
 
+        private Form mForm;
+
+        [SetUp]
+        public void Setup()
+        {
+            mForm = EmptyWindow.CreateForm();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            mForm.Dispose();
+        }
+
         [Test]
         public void Run()
         {
-            using (var form = EmptyWindow.CreateForm())
+            Device device;
+            SwapChain swapChain;
+            RenderTargetView renderTarget;
+
+            EmptyWindow.CreateDeviceSwapChainAndRenderTarget(mForm, out device, out swapChain, out renderTarget);
+
+            // buffer size 1 .. 4096 (0 defaults to 4096)
+            var sprite = new Sprite(device, 10);
+            var font = new Font(device, 20, "ARIAL");
+
+            Application.Idle +=
+                delegate
+                {
+                    device.ClearRenderTargetView(renderTarget, new Color4(1, 0, 0));
+                    
+                    var rectangle = new Rectangle(10, 10, 400, 300);
+                    sprite.Begin(SpriteFlags.None);
+                    font.Draw(sprite, "Hello from SlimDX", rectangle, FontDrawFlags.Left,
+                        (uint)Color.Yellow.ToArgb());
+                    sprite.End();
+
+                    swapChain.Present(0, PresentFlags.None);
+
+                    Application.DoEvents();
+                };
+
+            Application.Run(mForm);  
+        }
+
+        [Test]
+        public void Using_DirectWrite()
+        {
+            Device device;
+            SwapChain swapChain;
+            RenderTargetView renderTarget;
+
+            EmptyWindow.CreateDeviceSwapChainAndRenderTarget(mForm, out device, out swapChain, out renderTarget);
+
+            var direct2dfactory = new Direct2DFactory();
+            var directWriteFactory = new DirectWriteFactory();
+
+            var properties = new WindowRenderTargetProperties { Handle = mForm.Handle, PixelSize = mForm.ClientSize };
+            var windowRenderTarget = new WindowRenderTarget(direct2dfactory, properties);
+            var brush = new SolidColorBrush(windowRenderTarget, Color.Black);
+
+            var textFormat = new TextFormat(directWriteFactory, "Gabriola", 
+                FontWeight.Normal, FontStyle.Normal, FontStretch.Normal, 72.0f, "en-us")
             {
-                Device device;
-                SwapChain swapChain;
-                RenderTargetView renderTarget;
+                TextAlignment = TextAlignment.Center,
+                ParagraphAlignment = ParagraphAlignment.Center
+            };
 
-                EmptyWindow.CreateDeviceSwapChainAndRenderTarget(form, out device, out swapChain, out renderTarget);
+            Application.Idle +=
+                delegate
+                {
+                    device.ClearRenderTargetView(renderTarget, new Color4(1, 0, 0));
 
-                // buffer size 1 .. 4096 (0 defaults to 4096)
-                var sprite = new Sprite(device, 10);
-                var font = new Font(device, 20, "ARIAL");
-
-                Application.Idle +=
-                    delegate
+                    if (!windowRenderTarget.IsOccluded)
                     {
-                        device.ClearRenderTargetView(renderTarget, new Color4(1, 0, 0));
-                        
-                        var rectangle = new Rectangle(10, 10, 400, 300);
-                        sprite.Begin(SpriteFlags.None);
-                        font.Draw(sprite, "Hello from SlimDX", rectangle, FontDrawFlags.Left,
-                            (uint)Color.Yellow.ToArgb());
-                        sprite.End();
+                        windowRenderTarget.BeginDraw();
+                        windowRenderTarget.Transform = Matrix3x2.Identity;
+                        windowRenderTarget.Clear(Color.White);
 
-                        swapChain.Present(0, PresentFlags.None);
+                        windowRenderTarget.DrawText("Hello World using DirectWrite!", textFormat, mForm.ClientRectangle, brush);
 
-                        Application.DoEvents();
-                    };
+                        windowRenderTarget.EndDraw();
+                    }
 
-                Application.Run(form);
-            }   
+                    swapChain.Present(0, PresentFlags.None);
+
+                    Application.DoEvents();
+                };
+
+            Application.Run(mForm);
         }
     }
 }
